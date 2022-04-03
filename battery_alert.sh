14cbe1bb1600d6c28c432952d6640ef152383029
Sankar <sankar.hari@outlook.com>
2022-04-03 21:27:44 +0530

    fix the alert conditions

--- battery_alert.sh
+++ battery_alert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 beacon()
 {
-  SOUND_FILE="/opt/BatterAlert/alien_beacon.wav"
+  SOUND_FILE="/opt/BatteryAlert/alien_beacon.wav"
   if [ "$1" -eq "1" -a $2 -gt 0 -a -f $SOUND_FILE ]
   then
     for i in {1..3}
@@ -32,16 +32,24 @@ do
   FLAG=`dpkg --get-selections sox | awk '{print match($0,"install")}'`
   CHARGE_PERCENT=`upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage"`
   BATTERY_STATE=`upower -i $(upower -e | grep BAT) | grep --color=never -E "state"`
-  GET_PERCENT=$(echo $CHARGE_PERCENT | cut -c13-14)
+#  echo $CHARGE_PERCENT
+#  echo $BATTERY_STATE
+  GET_PERCENT=$(echo $CHARGE_PERCENT | cut -c13-16)
+  GET_PERCENT2=$(echo $GET_PERCENT | awk -F'%' '{print $1}')
   GET_STATE=$(echo $BATTERY_STATE | cut -d' ' -f 2)
-  if [ $GET_STATE = "discharging" -a $GET_PERCENT -le 30 ]
+  echo $GET_PERCENT
+  echo $GET_PERCENT2
+  echo $GET_STATE
+  echo "--------"
+  if [[ $GET_STATE = "discharging" && $GET_PERCENT2 -le 10 ]]
   then
-    notify-send -u normal -i battery -t 5000 "$(echo -e "Your Laptop charge is less than 30%.\nPlease turn on power switch.")"
+    notify-send -u normal -i battery -t 5000 "$(echo -e "Your Laptop charge is less than 10%.\nPlease turn on power switch.")"
     beacon 1 $FLAG
-  elif [ $GET_STATE = "charging" -a $GET_PERCENT -ge 90 ]
+  elif [[ ($GET_PERCENT2 -ge 99  && $GET_STATE = "fully-charged") ]]
   then
-    notify-send -u normal -i battery -t 5000 "$(echo -e "Your Laptop charge is more than 90%.\nPlease turn off power switch.")"
+    notify-send -u normal -i battery -t 5000 "$(echo -e "Your Laptop charge is more than 99%.\nPlease turn off power switch.")"
     beacon 1 $FLAG
   fi
+  #elif [[ ($GET_STATE = "fully-charged" && $GET_PERCENT -eq 10) || ($GET_PERCENT -ge 99  && $GET_STATE = "fully-charged") ]]
   sleep 10
 done
